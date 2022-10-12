de8843612feb9e28f7dd2ab8fd2491fce73df27a
William <fpmw.50@gmail.com>
2022-10-12 15:06:03 -0500

    Agregar archivos de proyecto.

---
+++ PruebaGithub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esta es una prueba Github");
+            Console.ReadLine();
+
+        }
+    }
+}
